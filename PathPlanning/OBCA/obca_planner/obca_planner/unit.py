fc4bad0ca60636a6de6d706b7fad0338e42b5e11
lixianqiang <lxq243808918@gmail.com>
2023-09-20 14:15:17 +0800

    refactor(obca_planner): 代码重构（2）

--- PathPlanning/OBCA/obca_planner/obca_planner/unit.py
+++ PathPlanning/OBCA/obca_planner/obca_planner/unit.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import tf_transformations
+
 
 def AngleDiff(endAngle, startAngle):
     deltaAngle = endAngle - startAngle
@@ -11,8 +13,10 @@ def AngleDiff(endAngle, startAngle):
         diffAngle = deltaAngle
     return diffAngle
 
+
 def CalcDistance(p1, p2):
     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
 
 def CalcCurvature(curr_p, prev_p, next_p):
     denominator = CalcDistance(prev_p, curr_p) * CalcDistance(curr_p, next_p) * CalcDistance(prev_p, next_p)
@@ -20,6 +24,7 @@ def CalcCurvature(curr_p, prev_p, next_p):
         return 0
     return 2.0 * ((curr_p[0] - prev_p[0]) * (next_p[1] - prev_p[1]) - (curr_p[1] - prev_p[1]) * (
             next_p[0] - prev_p[0])) / denominator
+
 
 def IsShiftPoint(curr_p, prev_p, next_p):
     dot_product = (curr_p[0] - prev_p[0]) * (next_p[0] - curr_p[0]) + (curr_p[1] - prev_p[1]) * (
@@ -31,17 +36,6 @@ def IsShiftPoint(curr_p, prev_p, next_p):
         return True
     return False
 
-# def ConvertQuaternionToYaw(quaternion):
-#     quaternion /= np.linalg.norm(quaternion)
-#
-#     rotation_matrix = np.array([
-#         [1 - 2 * (quaternion[2]**2 + quaternion[3]**2), 2 * (quaternion[1] * quaternion[2] - quaternion[0] * quaternion[3]), 2 * (quaternion[1] * quaternion[3] + quaternion[0] * quaternion[2])],
-#         [2 * (quaternion[1] * quaternion[2] + quaternion[0] * quaternion[3]), 1 - 2 * (quaternion[1]**2 + quaternion[3]**2), 2 * (quaternion[2] * quaternion[3] - quaternion[0] * quaternion[1])],
-#         [2 * (quaternion[1] * quaternion[3] - quaternion[0] * quaternion[2]), 2 * (quaternion[2] * quaternion[3] + quaternion[0] * quaternion[1]), 1 - 2 * (quaternion[1]**2 + quaternion[2]**2)]
-#     ])
-#
-#     yaw_radians = math.atan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
-#     return yaw_radians
 
 # def ConvertQuaternionToYaw(quaternion):
 #     x, y, z, w = quaternion
@@ -58,14 +52,15 @@ def IsShiftPoint(curr_p, prev_p, next_p):
 #     quaternion[3] = math.cos(yaw / 2.0)
 #     return quaternion
 
-import tf_transformations
-
 def ConvertQuaternionToYaw(quaternion):
     _, _, yaw = tf_transformations.euler_from_quaternion(quaternion)
     return yaw
+
+
 def ConvertYawToQuaternion(yaw):
     quat = tf_transformations.quaternion_from_euler(0, 0, yaw)
     return quat
+
 
 def ExtractRectangularContourPoints(center_pose, contour_shape):
     x, y, theta = center_pose[0], center_pose[1], center_pose[2]
@@ -73,9 +68,9 @@ def ExtractRectangularContourPoints(center_pose, contour_shape):
     half_width = width / 2
     half_length = length / 2
 
-    x_bottom_left = x - half_length* math.cos(theta) - half_width * math.sin(theta)
+    x_bottom_left = x - half_length * math.cos(theta) - half_width * math.sin(theta)
     y_bottom_left = y - half_length * math.sin(theta) + half_width * math.cos(theta)
-    
+
     x_top_left = x + half_length * math.cos(theta) - half_width * math.sin(theta)
     y_top_left = y + half_length * math.sin(theta) + half_width * math.cos(theta)
 
@@ -85,7 +80,8 @@ def ExtractRectangularContourPoints(center_pose, contour_shape):
     x_bottom_right = x - half_length * math.cos(theta) + half_width * math.sin(theta)
     y_bottom_right = y - half_length * math.sin(theta) - half_width * math.cos(theta)
 
-    return [(x_top_left, y_top_left), (x_top_right, y_top_right), (x_bottom_right, y_bottom_right), (x_bottom_left, y_bottom_left), (x_top_left, y_top_left)]
+    return [(x_top_left, y_top_left), (x_top_right, y_top_right), (x_bottom_right, y_bottom_right),
+            (x_bottom_left, y_bottom_left), (x_top_left, y_top_left)]
 
 
 if __name__ == '__main__':
